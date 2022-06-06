d59c6bc954df0feb82deb559795b98fc7f2a1713
Jordan Alexander Cruz - AlexCG Design <jordanalexanderweb@gmail.com>
2022-06-06 11:41:21 -0500

    first commit

---
+++ Menu Desplegable/main.js
@@ -0,0 +1,17 @@
+let listElements = document.querySelectorAll('.list__button--click');
+
+listElements.forEach(listElement => {
+    listElement.addEventListener('click', ()=>{
+        
+        listElement.classList.toggle('arrow');
+
+        let height = 0;
+        let menu = listElement.nextElementSibling;
+        if(menu.clientHeight == "0"){
+            height=menu.scrollHeight;
+        }
+
+        menu.style.height = `${height}px`;
+
+    })
+});
